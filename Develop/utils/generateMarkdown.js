4cc3b6a4432f8ad595c7718914e9d3adea39afc2
bds6575 <brian.sanchez2693@gmail.com>
2024-06-02 19:41:46 -0500

    created arraty of questions for user input

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,3 +1,53 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('../Develop/utils/generateMarkdown'); 
+
+// Array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project title:',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter a description for your project:',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter installation instructions:',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter usage information:',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Choose a license for your application:',
+    choices: ['MIT', 'Apache 2.0', 'GNU GPL v3', 'Other'],
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'Enter contribution guidelines:',
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Enter test instructions:',
+  },
+  {
+    type: 'input',
+    name: 'githubUsername',
+    message: 'Enter your GitHub username:',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address:',
+  },
+];
